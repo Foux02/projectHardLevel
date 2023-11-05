0592c7bb1365d3c89f0b04365442dbf8b2bb6e88
Jane Lutak <foux02911@gmail.com>
2023-11-05 03:27:34 +0300

    Cоздание переменной. Вычисление произведения. Возведение в степень. Вывод первых двух чисел.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//Начинаем сложный уровень в dev.
+// Начинаем сложный уровень в dev. У первого урока не было сложного уровня
+
+const num = 266219;
+let numArray = ("" + num).split("").map(Number);
+
+console.log(numArray);
+
+let numMultiplication = numArray.reduce(function (result, num) {
+  return result * num;
+}, 1);
+
+console.log(numMultiplication);
+console.log(numMultiplication ** 3);
+console.log(String(numMultiplication ** 3).substr(0, 4));
