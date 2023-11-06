bd8af68f7f15a93afcbc18f9c38c5538a606b29f
Jane Lutak <foux02911@gmail.com>
2023-11-06 22:44:38 +0300

    Поменяла старую запись на StiString(num). Поменяла на slice

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Начинаем сложный уровень в dev. У первого урока не было сложного уровня
 
 const num = 266219;
-let numArray = ("" + num).split("").map(Number);
+let numArray = String(num).split("").map(Number);
 
 console.log(numArray);
 
@@ -11,4 +11,4 @@ let numMultiplication = numArray.reduce(function (result, num) {
 
 console.log(numMultiplication);
 console.log(numMultiplication ** 3);
-console.log(String(numMultiplication ** 3).substr(0, 2));
+console.log(String(numMultiplication ** 3).slice(0, 2));
