b4b3330ba7eea7078598217ecee5a70c275ff03e
Jane Lutak <foux02911@gmail.com>
2023-11-15 01:45:58 +0300

    Выполнила Урок 5 Задания 2 и 3

--- script.js
+++ script.js
@@ -4,13 +4,30 @@
 
 // Задание №2
 
-const arr = ['123', '456', '789', '279', '489', '124', '255'];
-let test = '0';
+const arr = [123, 281, 456, 789, 489, 124, 255];
+const arrString = arr.map(String);
 
-let str = arr.map(function (a) {
+let str = arrString.filter(function (a) {
   if (a[0] == 2 || a[0] == 4) {
-    return (test = test + ',' + a);
+    return a;
   }
 });
+console.log(str.map(Number));
 
-console.log(test.split(',').splice(1));
+// Задание №3
+
+for (let i = 2; i < 101; i++) {
+  if (
+    ((i / 2) % 1 === 0 && i > 2) ||
+    ((i / 3) % 1 === 0 && i > 3) ||
+    ((i / 4) % 1 === 0 && i > 4) ||
+    ((i / 5) % 1 === 0 && i > 5) ||
+    ((i / 6) % 1 === 0 && i > 6) ||
+    ((i / 7) % 1 === 0 && i > 7) ||
+    ((i / 8) % 1 === 0 && i > 8) ||
+    ((i / 9) % 1 === 0 && i > 9)
+  )
+    continue;
+
+  console.log(`Делители этого числа: 1 и ${i}`);
+}
