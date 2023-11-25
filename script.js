9d68b5d7ed6b93513e295c49bfebce66774bd311
Jane Lutak <foux02911@gmail.com>
2023-11-25 16:29:24 +0300

    Первая попытка в процессе

--- script.js
+++ script.js
@@ -1,75 +1,61 @@
 // Начинаем сложный уровень в dev. У первого урока не было сложного уровня
-"use strict";
-
-const lang = document.documentElement.lang;
-
-const num = 266219;
-const dayWeekRu = [
-  "Воскресенье",
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-];
-const dayWeekEn = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let numArray = String(num).split("").map(Number);
-let numMultiplication = numArray.reduce(function (result, num) {
-  return result * num;
-}, 1);
+'use strict';
+/*
 let nowDayWeek = new Date();
 
-if (lang == "ru") {
+let week = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье',
+];
+
+//let week2 = week.join('\n');
+
+//document.body.prepend(document.createTextNode(week2));
+
+//console.log(week2);
+
+for (let key in week) {
+  document.body.prepend(document.createTextNode(week[key]));
+}
+
+console.dir(document.childNodes);
+
+/*
+
+if (lang == 'ru') {
   document.body.prepend(
     document.createTextNode(dayWeekRu[nowDayWeek.getDay()])
   );
-} else if (lang == "en") {
+} else if (lang == 'en') {
   document.body.prepend(
     document.createTextNode(dayWeekEn[nowDayWeek.getDay()])
   );
 } else {
 }
+*/
+const week = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье',
+];
 
-switch (true) {
-  case lang == "ru":
-    document.body.prepend(
-      document.createTextNode(dayWeekRu[nowDayWeek.getDay()])
-    );
-    break;
+for (let i = 0, len = week.length; i < len; i++) {
+  let html = week[i];
+  if (i === 0) html = html.italics(); // понедельник
+  else if (i > 4) html = html.bold(); // выходные
 
-  default:
-    document.body.prepend(
-      document.createTextNode(dayWeekEn[nowDayWeek.getDay()])
-    );
-    break;
+  const p = document.createElement('div');
+  p.innerHTML = html;
+  document.body.appendChild(p);
 }
 
-lang == "ru"
-  ? document.body.prepend(
-      document.createTextNode(dayWeekRu[nowDayWeek.getDay()])
-    )
-  : document.body.prepend(
-      document.createTextNode(dayWeekEn[nowDayWeek.getDay()])
-    );
-
-let namePerson = "Сергей";
-namePerson == "Артем"
-  ? console.log("Директор")
-  : namePerson == "Александр"
-  ? console.log("Преподаватель")
-  : console.log("Студент");
-
-console.log(numArray);
-console.log(numMultiplication);
-console.log(numMultiplication ** 3);
-console.log(String(numMultiplication ** 3).slice(0, 2));
+console.dir(document.childNodes);
