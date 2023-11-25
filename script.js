04c775e73a9aa159b67427922aca1fcff8c2ff4b
Jane Lutak <foux02911@gmail.com>
2023-11-26 00:39:41 +0300

    1 и 2 задания зделаны

--- script.js
+++ script.js
@@ -1,61 +1,83 @@
-// Начинаем сложный уровень в dev. У первого урока не было сложного уровня
 'use strict';
-/*
-let nowDayWeek = new Date();
 
-let week = [
-  'Понедельник',
-  'Вторник',
-  'Среда',
-  'Четверг',
-  'Пятница',
-  'Суббота',
-  'Воскресенье',
-];
-
-//let week2 = week.join('\n');
-
-//document.body.prepend(document.createTextNode(week2));
-
-//console.log(week2);
-
-for (let key in week) {
-  document.body.prepend(document.createTextNode(week[key]));
-}
-
-console.dir(document.childNodes);
-
-/*
-
-if (lang == 'ru') {
-  document.body.prepend(
-    document.createTextNode(dayWeekRu[nowDayWeek.getDay()])
-  );
-} else if (lang == 'en') {
-  document.body.prepend(
-    document.createTextNode(dayWeekEn[nowDayWeek.getDay()])
-  );
-} else {
-}
-*/
 const week = [
+  'Воскресенье',
   'Понедельник',
   'Вторник',
   'Среда',
   'Четверг',
   'Пятница',
   'Суббота',
-  'Воскресенье',
 ];
 
-for (let i = 0, len = week.length; i < len; i++) {
-  let html = week[i];
-  if (i === 0) html = html.italics(); // понедельник
-  else if (i > 4) html = html.bold(); // выходные
+const month = [
+  'января',
+  'февраля',
+  'марта',
+  'апреля',
+  'мая',
+  'июня',
+  'июля',
+  'августа',
+  'сентября',
+  'октября',
+  'ноября',
+  'декабря',
+];
 
-  const p = document.createElement('div');
-  p.innerHTML = html;
-  document.body.appendChild(p);
+let nowDate;
+
+let hourText = function () {
+  if (nowDate.getHours() === 1 || nowDate.getHours() === 21) {
+    hourText = 'час';
+    console.log('1 или 21 ' + nowDate.getHours() + hourText);
+  } else if (
+    (nowDate.getHours() > 4 && nowDate.getHours() < 21) ||
+    nowDate.getHours() === 0
+  ) {
+    hourText = 'часов';
+    console.log('от 5 до 20 ' + nowDate.getHours() + hourText);
+  } else {
+    hourText = 'часа';
+    console.log('остальное ' + nowDate.getHours() + hourText);
+  }
+};
+/*
+function updateSite() {
+  nowDate = new Date();
+
+  if (nowDate.getHours() === 1 || nowDate.getHours() === 21) {
+    hourText = 'час';
+    console.log('1 или 21 ' + nowDate.getHours() + hourText);
+  } else if (
+    (nowDate.getHours() > 4 && nowDate.getHours() < 21) ||
+    nowDate.getHours() === 0
+  ) {
+    hourText = 'часов';
+    console.log('от 5 до 20 ' + nowDate.getHours() + hourText);
+  } else {
+    hourText = 'часа';
+    console.log('остальное ' + nowDate.getHours() + hourText);
+  }
+
+  document.body.append(
+    document.createTextNode(
+      `а) Сегодня ${week[nowDate.getDay()]}, ${nowDate.getDate()} ${
+        month[nowDate.getMonth()]
+      } ${nowDate.getFullYear()} года, ${nowDate.getHours()} ${hourText} ${nowDate.getMinutes()} минуты ${nowDate.getSeconds()} секунды
+    б) ${new Intl.DateTimeFormat('ru').format(
+      nowDate
+    )} - ${new Intl.DateTimeFormat('ru', {
+        timeStyle: 'medium',
+      }).format(nowDate)}`
+    )
+  );
 }
 
-console.dir(document.childNodes);
+//document.createTextNode('Некоторый текст');
+
+//hourText();
+setInterval(updateSite, 3000);
+//console.log(nowDate.getHours());
+//console.log(new Intl.DateTimeFormat('ru').format(nowDate));
+*/
